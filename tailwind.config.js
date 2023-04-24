bbcecebc3ee394e8f6c6e00503b17339676e50f9
jefry_nm <jefroy95@gmail.com>
2023-04-23 20:24:03 -0500

    Se finalizó el proyecto

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./public/index.html", "./src/**/*.{html,js}"],
+  darkMode: 'class', // or 'media' or 'class '
   theme: {
     extend: {
     backgroundImage: {
